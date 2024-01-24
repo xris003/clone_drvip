8b124b9a8cb9cfc4e4312af9254f6c78b24e2096
xris003 <chrisikariagabriel@gmail.com>
2024-01-24 14:42:19 +0100

    setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,10 @@
- 
+const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 🚨 Shutting Down....");
+  console.log(err.name, err.message);
+});
+
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
