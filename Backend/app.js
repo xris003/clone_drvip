2a8d5d0fd500d7c45814300582c16d64218c9765
xris003 <chrisikariagabriel@gmail.com>
2024-01-24 16:23:08 +0100

    route

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// const merchantRouter = require("./routes/merchantRoute");
+
+const app = express();
+
+app.get("/createddb", (req, res) => {
+  let sql = "CREATE DATABASE drvip";
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Database Created");
+  });
+});
+
+// app.use("/api/v1/merchants", merchantRouter);
+
+module.exports = app;
--- Backend/routes/merchantRoute.js
+++ Backend/routes/merchantRoute.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const merchantController = require("../controllers/merchantController");
-const authController = require("../controllers/authController");
+// const express = require("express");
+// const merchantController = require("../controllers/merchantController");
+// const authController = require("../controllers/authController");
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-  .route("/")
-  .get(authController.protect, merchantController.getAllMerchants)
-  .post(merchantController.createMerchants);
+// router
+//   .route("/")
+//   .get(authController.protect, merchantController.getAllMerchants)
+//   .post(merchantController.createMerchants);
 
-router.route("/:id").patch(merchantController.updateMerchants);
+// router.route("/:id").patch(merchantController.updateMerchants);
 
-module.exports = router;
+// module.exports = router;
