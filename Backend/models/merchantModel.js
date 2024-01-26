5f1a61119c90cc7119a794fabd6a79da0aa4938a
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-01-26 12:31:59 +0100

    sequelize

---
+++ Backend/models/merchantModel.js
@@ -0,0 +1,26 @@
+modules.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define("Users", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    confirmPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Users;
+};
