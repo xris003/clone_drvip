bb10f08b07c987a7a8a3824443f9389a412f536d
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-01-26 13:55:03 +0100

    server

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-// const merchantRouter = require("./routes/merchantRoute");
+// const express = require("express");
+// // const merchantRouter = require("./routes/merchantRoute");
 
-const app = express();
+// const app = express();
 
-app.get("/createddb", (req, res) => {
-  let sql = "CREATE DATABASE drvip";
-  db.query(sql, (err) => {
-    if (err) {
-      throw err;
-    }
-    res.send("Database Created");
-  });
-});
+// app.get("/createddb", (req, res) => {
+//   let sql = "CREATE DATABASE drvip";
+//   db.query(sql, (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send("Database Created");
+//   });
+// });
 
-// app.use("/api/v1/merchants", merchantRouter);
+// // app.use("/api/v1/merchants", merchantRouter);
 
-module.exports = app;
+// module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,22 @@
 const express = require("express");
-const mysql2 = require("mysql2");
-const dotenv = require("dotenv");
+// const mysql2 = require("mysql2");
+// const dotenv = require("dotenv");
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION! 🚨 Shutting Down....");
   console.log(err.name, err.message);
 });
 
-dotenv.config({ path: "./config.env" });
+// dotenv.config({ path: "./config.env" });
+const app = express();
 
-const app = require("./app");
+const db = require("/models");
 
-const db = mysql2.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "xris07026305657",
-  database: "drvip",
-});
-
-// Connect to MySQL
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  res.send("Database Created");
-});
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}... `);
+db.sequelize.sync().then(() => {
+  const port = process.env.PORT || 3000;
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}... `);
+  });
 });
 
 process.on("unhandledRejection", (err) => {
