eb15d5a0c19a2536b6f8616f744a23d4b1a066d7
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-02-01 14:32:19 +0100

    newConfig

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,51 +1,14 @@
-"use strict";
-
-const fs = require("fs");
-const path = require("path");
+const config = require("../config/config");
 const Sequelize = require("sequelize");
-const process = require("process");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+const sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.DIALECT,
 });
 
+const db = {};
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-db.models.User = require("./userModel")(sequelize, Sequelize.DataTypes);
+db.models = {};
+db.models.User = require("./user")(sequelize, Sequelize.DataTypes);
 
 module.exports = db;
