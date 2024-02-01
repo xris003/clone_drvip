1315eca511ae7f858f1b1efed146873dcbef3917
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-02-01 14:57:52 +0100

    newConfig

--- Backend/app.js
+++ Backend/app.js
@@ -5,16 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/createddb", (req, res) => {
-//   let sql = "CREATE DATABASE drvip";
-//   db.query(sql, (err) => {
-//     if (err) {
-//       throw err;
-//     }
-//     res.send("Database Created");
-//   });
-// });
-
 app.use("/api/v1/users", userRouter);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,15 @@
-// const express = require("express");
-// const mysql2 = require("mysql2");
-
-// process.on("uncaughtException", (err) => {
-//   console.log("UNCAUGHT EXCEPTION! 🚨 Shutting Down....");
-//   console.log(err.name, err.message);
-// });
-
-const app = require("./app");
-// const app = express();
-
 const db = require("./models");
 
-db.sequelize.sync().then(() => {
-  const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => {
-    console.log(`App running on port ${port}... `);
+const app = require("./app");
+
+(async () => {
+  db.sequelize.sync().then(() => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`App running on port ${port}... `);
+    });
   });
-});
+})();
 
 // process.on("unhandledRejection", (err) => {
 //   console.log("UNHANDLED REJECTION! 🚨 Shutting Down....");
