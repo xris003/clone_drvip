5b40d53a67d3f4fec890a4aa8970cb712dd2e705
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-02-02 22:51:50 +0100

    user&error&index

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,26 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const User = sequelize.define(
+    "user",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      confirmPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    confirmPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      freezeTableName: true,
+    }
+  );
 
   return User;
 };
