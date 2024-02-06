27ab1acc85f08d14d383970811f21497e86fa229
xris003 <chrisikariagabriel@gmail.com>
2024-02-06 17:00:08 +0100

    new

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userController = require("../controllers/userController");
+const userController = require("../controllers/user");
 // const authController = require("../controllers/authController");
 
 const router = express.Router();
