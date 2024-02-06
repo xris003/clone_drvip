ea82207f263acd0deedea9ab341142ecf4ecfb12
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-02-06 23:59:32 +0100

    getuser

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -18,6 +18,21 @@ exports.getAllUsers = async (req, res) => {
   });
 };
 
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ where: { id: req.params.id } });
+
+  if (!user) {
+    return next(new AppError("No document with that number", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: user,
+    },
+  });
+});
+
 exports.createUsers = async (req, res) => {
   const user = req.body;
   await User.create(user);
