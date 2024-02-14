7af06caff223ed19f20f7ffae18217f01b5d9455
xris003 <chrisikariagabriel@gmail.com>
2024-02-14 13:44:16 +0100

    loginupdate

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -28,5 +28,18 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
+  // Use a hook to hash the password before saving the user to the database
+  User.beforeCreate(async (user) => {
+    if (user.changed("password")) {
+      user.password = await bcrypt.hash(user.password, 12);
+      user.confirmPassword = undefined;
+    }
+  });
+
+  // Instance method to compare passwords
+  User.prototype.correctPassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+  };
+
   return User;
 };
