b6e3eb2f470257c987d3e2be0fdf227711eba4d9
xris003 <chrisikariagabriel@gmail.com>
2024-02-19 12:30:06 +0100

    merchant update

--- Backend/models/merchant.js
+++ Backend/models/merchant.js
@@ -1,53 +1,69 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequelize, DataTypes) => {
-  const Merchants = sequelize.define("merchant", {
-    businessName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Merchants = sequelize.define(
+    "merchant",
+    {
+      businessName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      businessEmail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      businessType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      businessAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      contact: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      owner: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      confirmPassword: {
+        type: DataTypes.VIRTUAL,
+        allowNull: false,
+        // defaultValue: "*********",
+        validate: {
+          isSamePassword(value) {
+            if (value !== this.password) {
+              throw new Error("Passwords do not match");
+            }
+          },
+        },
+      },
+      walletAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    businessEmail: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    businessType: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    businessAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    state: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    contact: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    owner: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    confirmPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    walletAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      freezeTableName: true,
+    }
+  );
 
   // Use a hook to hash the password before saving the user to the database
-  User.beforeCreate(async (merchant) => {
+  Merchants.beforeCreate(async (merchant) => {
     if (merchant.changed("password")) {
       merchant.password = await bcrypt.hash(merchant.password, 12);
       merchant.confirmPassword = undefined;
@@ -55,7 +71,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   // Instance method to compare passwords
-  User.prototype.correctPassword = async function (candidatePassword) {
+  Merchants.prototype.correctPassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
   };
 
