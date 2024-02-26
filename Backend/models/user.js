2c9be5be73f3afb9cc291a6b7e354dc1740993df
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-02-26 23:58:18 +0100

    updateusermodel

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -37,7 +37,9 @@ module.exports = (sequelize, DataTypes) => {
       passwordResetExpiresAt: {
         type: DataTypes.DATE,
       },
+      activated: {},
     },
+
     {
       freezeTableName: true,
       createdAt: false,
